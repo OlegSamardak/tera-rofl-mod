60886c89cc21aa0ad672d8b001c7b81336c05b85
Oleg Samardak <olegsamardak98@gmail.com>
2020-05-19 20:54:00 +0300

    add some basic func

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ module.exports = function RoflMod(mod) {
     mod.command.add('huy', (x, y, z) => {
         mod.command.message('pizda in : ' + [x, y, z].join(', '))
     })
+
+    mod.hook('S_SHOW_HP', 3, event => {
+        if (event.curHp == 0) {
+            mod.command.message('jebat ti loh');
+        }
+    })
 };
