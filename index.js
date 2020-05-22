0ee2eb9a3a13a8cf0d2ab235694b2a57fa03c863
Oleg Samardak <olegsamardak98@gmail.com>
2020-05-22 22:54:12 +0300

    log the mod

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function RoflMod(mod) {
 
     mod.game.me.on('die', () => {
         mod.log(mod.clientInterface.info.path + '/dancing_coffin.jpg');
+        mod.log(mod);
         mod.send("S_CUSTOM_STYLE_SYSTEM_MESSAGE", 1, {
             message: `<img src="img://${mod.clientInterface.info.path + '/dancing_coffin.jpg'}" width="300" height="300" vspace="-20"/><font size="24">&nbsp;Some really cool guys</font>`,
             style: mod.settings.resetStyle
