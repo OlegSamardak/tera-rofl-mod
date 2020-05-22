25f3e1a1f26cb60c69d2f2bcb53c971072136416
Oleg Samardak <olegsamardak98@gmail.com>
2020-05-23 00:13:22 +0300

    test icon

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function RoflMod(mod) {
         mod.log(mod.clientInterface.info.path + '/dancing_coffin.jpg');
         mod.log(mod);
         mod.send("S_CUSTOM_STYLE_SYSTEM_MESSAGE", 1, {
-            message: `<img src="img://__Icon_Skills.rapidshot_Tex'}" width="300" height="300" vspace="-20"/><font size="24">&nbsp;Some really cool guys</font>`,
+            message: `<img src="img://__Icon_Skills.rapidshot_Tex" width="300" height="300" vspace="-20"/><font size="24">&nbsp;Some really cool guys</font>`,
             style: 51
         });
     })
