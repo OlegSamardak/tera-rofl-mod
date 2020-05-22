a5be7c8b570cebec43d4f9b0679fe26d50b4ccdb
Oleg Samardak <olegsamardak98@gmail.com>
2020-05-22 22:07:41 +0300

    user mod.game

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 module.exports = function RoflMod(mod) {
-    mod.command.add('huy', (x, y, z) => {
-        mod.command.message('pizda in : ' + [x, y, z].join(', '))
-    })
+    mod.game.initialize("me");
 
-    mod.hook('S_SHOW_DEAD_UI', 2, event => {
+    mod.game.me.on('die', () => {
         mod.log(mod.clientInterface.info.path + '/dancing_coffin.jpg');
         mod.send("S_CUSTOM_STYLE_SYSTEM_MESSAGE", 1, {
-            message: `<img src="img://__${mod.clientInterface.info.path + '/dancing_coffin.jpg'}" width="48" height="48" vspace="-20"/><font size="24">&nbsp;Some really cool guys</font>`,
+            message: `<img src="img://__${mod.clientInterface.info.path + '/dancing_coffin.jpg'}" width="300" height="300" vspace="-20"/><font size="24">&nbsp;Some really cool guys</font>`,
             style: mod.settings.resetStyle
         });
     })
