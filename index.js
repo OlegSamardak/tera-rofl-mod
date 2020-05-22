35a6ce76efa400281235d7b6030056041b7321ad
Oleg Samardak <olegsamardak98@gmail.com>
2020-05-22 20:48:30 +0300

    added mapping

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function RoflMod(mod) {
         mod.command.message('pizda in : ' + [x, y, z].join(', '))
     })
 
-    mod.hook('S_SHOW_DEAD_UI', 1, event => {
+    mod.hook('S_SHOW_DEAD_UI', 2, event => {
         mod.log(event);
     })
 };
