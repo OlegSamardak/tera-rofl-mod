4698f8711d54c5fd6b5c782a1ec4d99fcfe587ef
Oleg Samardak <olegsamardak98@gmail.com>
2020-05-22 22:17:10 +0300

    mb fix for img

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function RoflMod(mod) {
     mod.game.me.on('die', () => {
         mod.log(mod.clientInterface.info.path + '/dancing_coffin.jpg');
         mod.send("S_CUSTOM_STYLE_SYSTEM_MESSAGE", 1, {
-            message: `<img src="img://__${mod.clientInterface.info.path + '/dancing_coffin.jpg'}" width="300" height="300" vspace="-20"/><font size="24">&nbsp;Some really cool guys</font>`,
+            message: `<img src="img://${mod.clientInterface.info.path + '/dancing_coffin.jpg'}" width="300" height="300" vspace="-20"/><font size="24">&nbsp;Some really cool guys</font>`,
             style: mod.settings.resetStyle
         });
     })
