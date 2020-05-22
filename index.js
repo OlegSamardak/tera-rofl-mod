d9085ab5f39156eb443584161a17ffdc89422234
Oleg Samardak <olegsamardak98@gmail.com>
2020-05-22 21:54:47 +0300

    remove not existing setting

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ module.exports = function RoflMod(mod) {
     })
 
     mod.hook('S_SHOW_DEAD_UI', 2, event => {
-        mod.log(mod.clientInterface.info.path);
+        mod.log(mod.clientInterface.info.path + '/dancing_coffin.jpg');
         mod.send("S_CUSTOM_STYLE_SYSTEM_MESSAGE", 1, {
-            message: `<img src="img://__${mod.clientInterface.info.path + '/dancing_coffin.jpg'}" width="48" height="48" vspace="-20"/><font size="24" color="${mod.settings.reset_font_color}">&nbsp;Some really cool guys</font>`,
+            message: `<img src="img://__${mod.clientInterface.info.path + '/dancing_coffin.jpg'}" width="48" height="48" vspace="-20"/><font size="24">&nbsp;Some really cool guys</font>`,
             style: mod.settings.resetStyle
         });
     })
